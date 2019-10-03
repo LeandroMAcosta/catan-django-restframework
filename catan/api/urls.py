c3848ab75c1a8a0c259794fa3d9a332782b77a97
rojomaldit <lucaserfurt@hotmail.com>
2019-10-03 15:55:25 -0300

    Fixed errors

--- catan/api/urls.py
+++ catan/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('rooms/<id>', include('lobby.urls')),
+
 ]
--- catan/lobby/models.py
+++ catan/lobby/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class ROOM(models.Model):
+class Room(models.Model):
     #id = models.IntegerField(unique=True, primary_key=True)
     name = models.CharField(max_length=100)
     owner = models.CharField(max_length=100) #Change to USERNAME
     players = [] #Arrays of USERNAMEs
     max_players = models.IntegerField(default=4)
 
-    def join_room(p):
+    def join_room(player):
         if(len(this.players) < this.max_players):
-            this.players.append(p)
+            this.players.append(player)
 
