cc1cef4e12962ac55f8b9120a9bf5853ef2c7a0a
Leandro <leacosta97@gmail.com>
2019-10-03 14:36:40 -0300

    Change login url

--- catan/api/urls.py
+++ catan/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path('login/', obtain_jwt_token),
+    path('users/login/', obtain_jwt_token),
 ]
