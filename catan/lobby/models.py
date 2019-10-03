0dcc304a7a5478c16a5cd2f7ba1651be23629aca
rojomaldit <lucaserfurt@hotmail.com>
2019-10-03 14:34:28 -0300

    Create Room model

--- catan/lobby/models.py
+++ catan/lobby/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ROOM(models.Model):
+    #id = models.IntegerField(unique=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100) #Change to USERNAME
+    players = [] #Arrays of USERNAMEs
+    max_players = models.IntegerField(default=4)
+
+    def join_room(p):
+        if(len(this.players) < this.max_players):
+            this.players.append(p)
+
