54b994d3f408da546d77045cbbfa54a912b07f85
gongigena <gongigena@hotmail.com>
2019-10-03 18:07:52 -0300

    List of Rooms done

--- catan/lobby/views.py
+++ catan/lobby/views.py
@@ -4,19 +4,13 @@ from rest_framework.views import APIView
 from .models import Room
 
 class RoomsView(APIView):
+
     def get(self, request):
-        print("ghola, llegue aca bro")
         rooms = Room.objects.all()
-        return Response({"rooms": rooms})
+        list_rooms  = [room.name for room in rooms]
+        return Response(list_rooms)
+
+    def put(self, request):
+        pass
 
 
-
-"""
-def join_room(self, request):
-
-        print(request)
-        # request.user
-
-        # if(len(this.players) < this.max_players):
-        #     this.players.append(player)
-"""
