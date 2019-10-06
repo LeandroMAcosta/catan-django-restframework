8e1c777ea1851d563ab269c05eeededfbc505377
BustamanteM <malena_b_@hotmail.com.ar>
2019-10-06 20:13:27 -0300

    incomplete view

---
+++ catan/resources/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here. 
+class Resources(models.Model):
+    WOOL = 'WO'
+    LUMBER = 'LU'
+    BRICK = 'BR'
+    GRAIN = 'GR'
+    ORE = 'OR'
+    NOTHING = 'NO'
+    RESOURCES_CHOICES = [
+        (WOOL='Wool'),
+        (LUMBER='Lumber'),
+        (BRICK='Brick'),
+        (GRAIN='Grain'),
+        (ORE='Ore'),
+        (NOTHING='Nothing'),
+    ]
+    type_resource = models.CharField(
+        choices=RESOURCES_CHOICES,
+        default=NOTHING,
+        )
+    count_resource = models.IntegerField(default=0)
+    owner_player = models.ForeignKey(
+        Player, #app
+        on_delete=models.CASCADE,
+        )
---
+++ catan/resources/views.py
@@ -0,0 +1,15 @@
+from django.views import View
+from django.shortcuts import render
+
+
+# Create your views here.
+from .models import Resources
+
+
+class ResourcesView(APIView):
+
+
+    def get(self, request, plyr):
+        resource = Resources.objects.filter(owner_player=plyr)
+        list_resource  = [resources.id for resources in resource]
+        return Response(list_resource)
