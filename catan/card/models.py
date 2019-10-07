56fa4b5048ec253b387f60a6e1ef13ac66cb2419
Leandro <leacosta97@gmail.com>
2019-10-07 20:43:11 -0300

    Fix errors

--- catan/card/models.py
+++ catan/card/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from player.models import Player
 
 
 class Card(models.Model):
     CARD_TYPE = [
-        ('rb', 'Road building'),
-        ('yp', 'Year of plenty'),
-        ('m', 'Monopoly'),
-        ('vp', 'Victory point'),
-        ('k', 'Knight')
+        ('road_building', 'Road building'),
+        ('year_of_plenty', 'Year of plenty'),
+        ('monopoly', 'Monopoly'),
+        ('victory_point', 'Victory point'),
+        ('knight', 'Knight')
     ]
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     card_type = models.CharField(max_length=2, choices=CARD_TYPE)
