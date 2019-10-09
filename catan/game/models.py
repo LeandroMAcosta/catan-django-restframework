fec3493c787da2ed3321d19004a33247cc0aa234
MateoOstorero <mateoostorero71@gmail.com>
2019-10-09 12:10:16 -0300

    FIX PEP8

--- catan/game/models.py
+++ catan/game/models.py
@@ -32,7 +32,8 @@ class Hex(models.Model):
     # game_id will be a Foreign key to a board/game/room
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
     position = models.ForeignKey(VertexPosition, on_delete=models.CASCADE)
-    resource = models.CharField(max_length=10, choices=RESOURCES, default='nothing')
+    resource = models.CharField(max_length=10, choices=RESOURCES,
+                                default='nothing')
     token = models.PositiveIntegerField(default=0)
 
     class Meta:
