35052d4761f848c1e2821eeda2a0bfc8caea4492
Leandro <leacosta97@gmail.com>
2019-10-08 22:28:23 -0300

    Add seeder and modify migrate.sh

--- catan/migrate.sh
+++ catan/migrate.sh
@@ -7,7 +7,14 @@ rm db.sqlite3
 python manage.py makemigrations card player game
 python manage.py migrate
 
-python manage.py flush
+echo "yes" | python manage.py flush
+
+echo $'\n--------------------------------------'
+echo "-- Creando usuario Admin:"
+echo "-- User: admin"
+echo "-- password: admin123"
+echo "--------------------------------------"
 
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
 
+python manage.py loaddata catan/seeder/initial_data.json
