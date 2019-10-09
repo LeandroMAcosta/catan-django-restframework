e8899e0b9ca17d1c6ef4841c0c7e2a205ca00b61
rojomaldit <lucaserfurt@hotmail.com>
2019-10-09 12:10:11 -0300

    Solve error with pep8

--- catan/card/models.py
+++ catan/card/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from player.models import Player
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import permissions, viewsets, status
+from game.serializers import GameSerializer
+from card.serializers import CardSerializer
+from card.models import Card
 
 
 class Card(models.Model):
+
     CARD_TYPE = [
         ('road_building', 'Road building'),
         ('year_of_plenty', 'Year of plenty'),
@@ -10,15 +17,9 @@ class Card(models.Model):
         ('victory_point', 'Victory point'),
         ('knight', 'Knight')
     ]
+
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     card_type = models.CharField(max_length=30, choices=CARD_TYPE)
 
     def __str__(self):
         return self.card_type
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import permissions, viewsets, status
-from game.serializers import GameSerializer
-from card.serializers import CardSerializer
-from card.models import Card
-# from resource.models import Resource
