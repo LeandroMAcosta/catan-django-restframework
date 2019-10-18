ccf20df063f75156a942847ac049d6bde59d658e
MateoOstorero <mateoostorero71@gmail.com>
2019-10-18 16:00:14 -0300

    Give error messages to board

--- catan/game/views.py
+++ catan/game/views.py
@@ -14,9 +14,20 @@ class HexListViewSets(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
     def list(self, request, game_id):
+        if Game.objects.filter(id=game_id).count() == 0:
+            return Response({'Error': 'Game does not exists'},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
         queryset = Hex.objects.filter(game_id=game_id)
         serializer = HexSerializer(queryset, many=True)
-        return Response({'hexes': serializer.data})
+        size = queryset.count()
+        if size == 0:
+            return Response({'Error': 'Empty Board'},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
+        elif size != 19:
+            return Response({'Error': 'Board incomplete',
+                             'Hexes': serializer.data},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
+        return Response({'hexes': serializer.data}, status=status.HTTP_200_OK)
 
 
 class GameViewSets(viewsets.ModelViewSet):
@@ -27,7 +38,7 @@ class GameViewSets(viewsets.ModelViewSet):
     def list_cards_and_resources(self, request, game_id):
         try:
             game = Game.objects.get(id=game_id)
-            player = Player.objects.get(user=request.user, game=game)            
+            player = Player.objects.get(user=request.user, game=game)
             cards = Card.objects.filter(player=player)
             resources = Resource.objects.filter(player=player)
             data = {'cards': cards, 'resources': resources}
