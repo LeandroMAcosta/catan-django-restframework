9f3574936ea58269fc0cf12db8c346ab900a9f34
Leandro Acosta <leacosta97@gmail.com>
2019-10-19 15:16:37 -0300

    Add serializer

--- catan/user/serializers.py
+++ catan/user/serializers.py
@@ -12,6 +12,39 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class UserSerializerWithToken(serializers.ModelSerializer):
+
+    token = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True)
+    # user = serializers.CharField(label=_("User"))
+
+    def get_token(self, user):
+        token = Token.objects.create(user=user)
+        return str(token)
+
+    def validate(self, attrs):
+        username = attrs.get('user')
+        password = attrs.get('password')
+        print("\n\n\n\n\n\n")
+        print(username, password)
+        print("\n\n\n\n\n\n")
+        attrs['user'] = username
+        return attrs
+
+
+    # def create(self, validated_data):
+    #     password = validated_data.pop('password', None)
+    #     username = validated_data['user']
+    #     print("\n\n\n\n\nvalidated_data:\n")
+    #     print(validated_data)
+    #     print("\n\n\n\n\n\n")
+    #     instance = User(username=username)
+    #     if password is not None:
+    #         instance.set_password(password)
+    #     instance.save()
+    #     return instance
+
+
 class CustomAuthTokenSerializer(serializers.Serializer):
     user = serializers.CharField(label=_("User"))
 
@@ -28,12 +61,12 @@ class CustomAuthTokenSerializer(serializers.Serializer):
         password = attrs.get('pass')
 
         if username and password:
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
+            user = authenticate(
+                request=self.context.get('request'),
+                username=username,
+                password=password
+            )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = _('Unable to log in with provided credentials.')
                 raise serializers.ValidationError(msg, code='authorization')
