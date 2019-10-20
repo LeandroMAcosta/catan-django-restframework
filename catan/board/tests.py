8ddd8f97fd0c4dcef5417fa9a0345d6e534a3db7
MateoOstorero <mateoostorero71@gmail.com>
2019-10-20 02:09:18 -0300

    Fix tests

--- catan/board/tests.py
+++ catan/board/tests.py
@@ -1,45 +1,51 @@
 from django.test import TestCase
-from board.models import Board
+from django.contrib.auth import get_user_model
+from board.models import Board, Vertex, Hexagon
+from game.models import Game
+
+User = get_user_model()
 
 
 class BoardTest(TestCase):
-    def SetUp(self):
-        self.USER_USERNAME = "testuser"
-        self.USER_EMAIL = "testuser@test.com"
-        self.USER_PASSWORD = "supersecure"
-        self.GAME_ID = 666
-        self.PLAYER_ID = 667
-
+    def setUp(self):
         # Create user
         user_data = {
-            "username": self.USER_USERNAME,
-            "email": self.USER_EMAIL,
-            "password": self.USER_PASSWORD,
+            "username": "testuser",
+            "email": "testuser@test.com",
+            "password": "supersecure",
         }
-        user = User._default_manager.create_user(**user_data)
-        user.save()
+        self.user = User._default_manager.create_user(**user_data)
+        self.user.save()
+        self.game = Game.objects.create()
+        self.game.save()
 
-    # def test_create_board(self):
-    #     board = Board.objects.create()
+    def test_create_board(self):
+        board = Board.objects.create(owner=self.user, name="TestBoard")
+        board.save()
+        self.assertNotEqual(board, None)
+        self.assertEqual(board.owner, self.user)
+        self.assertEqual(board.name, "TestBoard")
 
     def test_create_vertex(self):
-        pass
-        # v = Vertex.objects.create(index=1, level=2)
-        # self.assertNotEqual(v, None)
-        # self.assertEqual(v.index, 1)
-        # self.assertEqual(v.level, 2)
+        v = Vertex.objects.create(game=self.game, level=1, index=2, used=True)
+        v.save()
+        self.assertNotEqual(v, None)
+        self.assertEqual(v.game, self.game)
+        self.assertEqual(v.level, 1)
+        self.assertEqual(v.index, 2)
+        self.assertEqual(v.used, True)
 
     def test_create_hex(self):
-        pass
-        # g = Game.objects.create()
-        # v = Vertex.objects.create(level=0, index=0)
-        # h = Hexagon.objects.create(game=g, position=v, token=2,
-        #                        resource='brick')
-        # self.assertNotEqual(h, None)
-        # self.a*ssertEqual(h.game, g)
-        # self.assertEqual(h.position, v)
-        # self.assertEqual(h.token, 2)
-        # self.assertEqual(h.resource, 'brick')
+        b = Board.objects.create(owner=self.user, name="Placeholder")
+        h = Hexagon.objects.create(board=b, level=1, index=2, token=2,
+                                   resource='brick')
+        h.save()
+        self.assertNotEqual(h, None)
+        self.assertEqual(h.board, b)
+        self.assertEqual(h.level, 1)
+        self.assertEqual(h.index, 2)
+        self.assertEqual(h.token, 2)
+        self.assertEqual(h.resource, 'brick')
 
     def test_hex_list(self):
         pass
@@ -53,7 +59,8 @@ class BoardTest(TestCase):
         # for i in range(0, 3):
         #     for j in range(0, 6*i):
         #         v = Vertex.objects.create(index=j, level=i)
-        #         h = Hexagon.objects.create(game=self.game, position=v, token=0,
+        #         h = Hexagon.objects.create(game=self.game,
+        # position=v, token=0,
         #                                resource='lumber')
         # view = HexListViewSets.as_view({'get': 'list'})
         # factory = APIRequestFactory()
