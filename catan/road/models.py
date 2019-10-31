6e0d7d536c3062e72fa3230e55a3cdaeea890566
MateoOstorero <mateoostorero71@gmail.com>
2019-10-31 17:04:45 -0300

    Brought new road/models.py file from another brach

--- catan/road/models.py
+++ catan/road/models.py
@@ -11,6 +11,12 @@ class Road(models.Model):
     v2 = models.ForeignKey(
         Vertex, on_delete=models.CASCADE, related_name='road_out')
 
+    def get_owner(self):
+        return self.owner
+
+    def get_game(self):
+        return self.owner.get_game()
+
     def __str__(self):
         return "Road {0} {1} {2}"
         return "Road " + str(self.v1) + " " + str(self.v2) + " from "
@@ -18,9 +24,10 @@ class Road(models.Model):
 
     def clean(self):
         road_v1_v2 = Road.objects.filter(
-            v1=self.v1, v2=self.v2).exclude(id=self.id)
+            v1=self.v1, v2=self.v2,
+            owner__game=self.get_game()).exclude(id=self.id)
         road_v2_v1 = Road.objects.filter(
-            v1=self.v2, v2=self.v1)
+            v1=self.v2, v2=self.v1, owner__game=self.get_game())
         if road_v1_v2.exists() or road_v2_v1.exists():
             raise ValidationError({'Exception': 'Edge already in use'})
 
