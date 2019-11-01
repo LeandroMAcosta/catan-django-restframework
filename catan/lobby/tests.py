f7ccf7ddb7d05bccfca5b718d677e0310f431db3
Mateo Ostorero <mat_ost@hotmail.com>
2019-11-01 02:46:26 +0000

    Revert "Added code to stop conflicts"
    This reverts commit a6dfc29c091bc771a3cafc23287ae2bac1212795

--- catan/lobby/tests.py
+++ catan/lobby/tests.py
@@ -37,7 +37,6 @@ class RoomTest(TestCase):
         }
         board = Board(**board_data)
         board.save()
-        board.hexagon_set.create()
         return board
 
     def create_room(self, name, board, user, max_players, gs):
--- catan/road/models.py
+++ catan/road/models.py
@@ -21,17 +21,5 @@ class Road(models.Model):
         return "Road " + str(self.v1) + " " + str(self.v2) + " from "
         + str(self.owner)
 
-<<<<<<< HEAD
-=======
-    def clean(self):
-        road_v1_v2 = Road.objects.filter(
-            v1=self.v1, v2=self.v2,
-            owner__game=self.get_game()).exclude(id=self.id)
-        road_v2_v1 = Road.objects.filter(
-            v1=self.v2, v2=self.v1, owner__game=self.get_game())
-        if road_v1_v2.exists() or road_v2_v1.exists():
-            raise ValidationError({'Exception': 'Edge already in use'})
-
->>>>>>> 3856720d927dc57f826e8332e1cfda5f161ff01a
     class Meta:
         unique_together = ['owner', 'v1', 'v2']
